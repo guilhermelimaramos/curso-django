464dfc06e619a8266dcc39b8f1491f50178f0b11
Guilherme Lima <glimaramos013@gmail.com>
2024-03-13 17:11:01 -0300

    created header and search button

--- first-project-django/recipes/views.py
+++ first-project-django/recipes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', status=201, context= {
+    return render(request, 'recipes/pages/home.html', status=201, context= {
         'name': 'Guilherme'
     })
