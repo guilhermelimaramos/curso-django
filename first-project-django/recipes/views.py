920eba8a329f2e0f82883c7823060e813d8ccae5
Guilherme Lima <glimaramos013@gmail.com>
2024-03-22 09:00:32 -0300

    Injecting example data

--- first-project-django/recipes/views.py
+++ first-project-django/recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', status=201, context= {
-        'name': 'Guilherme'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', status=201, context= {
-        'name': 'Guilherme'
+        'recipe': make_recipe()
     })
     
